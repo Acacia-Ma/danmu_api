1d6e96b7183e149a2b9e41638e808cafc5ad861a
xiaohanys <201495013@qq.com>
2026-01-12 10:51:42 +0100

    更新 UI

--- danmu_api/ui/template.js
+++ danmu_api/ui/template.js
@@ -105,6 +105,8 @@ export const HTML_TEMPLATE = /* html */ `
                             <option value="matchAnime">匹配动漫 - /api/v2/match</option>
                             <option value="getBangumi">获取番剧详情 - /api/v2/bangumi/:animeId</option>
                             <option value="getComment">获取弹幕 - /api/v2/comment/:commentId</option>
+                            <option value="getCommentByUrl">从URL获取弹幕 - /api/v2/comment?url=videoUrl&format=json</option>
+                            <option value="getAnimeByDouban">从豆瓣ID获取弹幕 - /api/v2/douban?douban_id=doubanId&episode_number=episodeNumber</option>
                             <option value="getSegmentComment">获取分片弹幕 - /api/v2/segmentcomment</option>
                         </select>
                     </div>
